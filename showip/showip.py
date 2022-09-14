1c47ed1cc51d8372d6bd046019c162457e65ba4c
claire_lai <asd207641@gmail.com>
2022-09-14 12:18:13 +0800

    cc

---
+++ showip/showip.py
@@ -0,0 +1,12 @@
+import os
+from subprocess import *
+cmd = "vtysh -c 'show ip route 27.50.0.82'"
+#os.system(cmd)
+result = os.popen(cmd)
+#result = os.system("vtysh -c 'show ip route 27.50.0.82'")
+#print(result)
+res = result.read()
+for line in res.splitlines():
+    if "*" in line:
+        nh = line.split()[1]
+        print(nh)
